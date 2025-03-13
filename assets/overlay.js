eda5604ffa076a137bc5d415464d28a85220676a
deboraptor <vdzdebora@gmail.com>
2025-03-13 20:29:14 +0100

    :sparkles: new script for navbar

---
+++ assets/overlay.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "250px"; // Ouvre l'overlay
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0"; // Ferme l'overlay
+}
